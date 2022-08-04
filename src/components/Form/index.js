328bddea2e15f2b991fe182166a3b7bf1dc42bc7
Felipe martins <felipeaxandre936@gmail.com>
2022-08-03 21:31:12 -0300

    pegando o valor digitado pelo usuário

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./form.css";
 
 export const Form = () => {
+  const [valueInput, setValue] = useState("");
+
+  const getInputValue = (event) => {
+    setValue(event.target.value);
+  };
   return (
-    <form className="form">
+    <form
+      className="form"
+      onSubmit={(event) => {
+        event.preventDefault();
+        setValue("");
+      }}
+    >
       <div className="search-bar">
         <input
+          value={valueInput}
+          onChange={getInputValue}
           type="text"
           placeholder="Digite uma tarefa"
           className="input-search"

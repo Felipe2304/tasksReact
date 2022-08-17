c663540526ec3f51feeac09da0afbe42657cd573
Felipe2304 <felipeaxandre936@gmail.com>
2022-08-17 00:13:50 -0300

    localstorage finalizado

--- src/App.js
+++ src/App.js
@@ -6,18 +6,30 @@ import { NotTasks } from "./components/NotTasks";
 import { ProgressBar } from "./components/ProgressBar";
 import { TasksItem } from "./components/TasksItem";
 
+const getTasks = ()=>{
+  const tasksString = localStorage.getItem('task')
+  if(!tasksString) return []
+  return JSON.parse(tasksString)
+}
 const App = () => {
-  const [listTasks, setListTasks] = useState([]);
+  const [listTasks, setListTasks] = useState(getTasks());
 
   const create = (taskText, completed = false) => {
-    return  setListTasks([
-      ...listTasks,
-      { id: Math.random() * 999, title: taskText, completed: completed },
-    ]);
+    const newTask = {
+      id: Math.random() * 999,
+      title: taskText,
+      completed: completed,
+    };
+    const newTaskList = [...listTasks, newTask];
+    setListTasks(newTaskList);
+    localStorage.setItem('task', JSON.stringify(newTaskList))
   };
+
   const deleteTask = (id) => {
+
     const newListFiltered = listTasks.filter((task) => task.id !== id);
     setListTasks(newListFiltered);
+    localStorage.setItem('task' , JSON.stringify(newListFiltered))
   };
 
   const toggleTask = (id) => {
@@ -28,38 +40,38 @@ const App = () => {
     });
 
     setListTasks(listCompleted);
+    localStorage.setItem('task', JSON.stringify(listCompleted))
   };
 
   return (
     <div className="container-app">
       <div className="content">
+        <Header />
+        <Form createTask={create} />
 
-      <Header />
-      <Form createTask={create} />
+        {!!listTasks.length && <ProgressBar listTasks={listTasks} />}
 
-      {!!listTasks.length && <ProgressBar listTasks={listTasks}  />}
+        <h4 className="task-all-text">Todas Tarefas</h4>
 
-      <h4 className="task-all-text">Todas Tarefas</h4>
-
-      <section className="task-box">
-        {!listTasks.length && <NotTasks />}
-        {!!listTasks.length && (
-          <ul className="list-tasks">
-            {listTasks.map((task) => {
-              return (
-                <TasksItem
-                  key={task.id}
-                  id={task.id}
-                  title={task.title}
-                  completed={task.completed}
-                  deleteTask={deleteTask}
-                  toggleTask={toggleTask}
-                />
-              );
-            })}
-          </ul>
-        )}
-      </section>
+        <section className="task-box">
+          {!listTasks.length && <NotTasks />}
+          {!!listTasks.length && (
+            <ul className="list-tasks">
+              {listTasks.map((task) => {
+                return (
+                  <TasksItem
+                    key={task.id}
+                    id={task.id}
+                    title={task.title}
+                    completed={task.completed}
+                    deleteTask={deleteTask}
+                    toggleTask={toggleTask}
+                  />
+                );
+              })}
+            </ul>
+          )}
+        </section>
       </div>
     </div>
   );
